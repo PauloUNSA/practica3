b3e14fcd06b252d217b4cbb0683df70dc2f07ad4
Joel <jchinop@unsa.edu.pe>
2023-06-17 07:12:05 -0500

    metodo de flotar e insertar

--- src/MaxHeap.java
+++ src/MaxHeap.java
@@ -1,8 +1,9 @@
 import java.util.ArrayList;
 
-// Implementación del TAD Heap genérico
+// Implementación del TAD Heap maximo
 class MaxHeap<E extends Comparable<E>> implements Heap<E>{
     private ArrayList<E> heap;
+    private int size;
 
     public MaxHeap() {
         this.heap = new ArrayList<>();
@@ -10,14 +11,42 @@ class MaxHeap<E extends Comparable<E>> implements Heap<E>{
 
     @Override
     public void insert(E e) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'insert'");
+        heap.add(e);
+        size++;
+        ascend(size-1);
+               
+    }
+
+    private void ascend(int i) {
+        int parent = (i - 1) / 2;
+        while (i>0 && (heap.get(i).compareTo(heap.get(parent)) <= 0)) {
+            swap(i, parent);
+            i = parent;
+            parent = (i - 1) / 2;
+        }
     }
 
     @Override
     public E remove() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'remove'");
+        if (isEmpty()) {
+            throw new NullPointerException("El heap está vacío");
+        }
+        E temp = heap.get(0);
+        heap.set(0, heap.remove(size-1));
+
+        descend(0)
+
+        return temp;
+    }
+
+    private void descend (int i) {
+        
+    }
+
+    private void swap (int i, int k) {
+        E temp = heap.get(i);
+        heap.set(i, heap.get(k));
+        heap.set(k, temp);
     }
 
     @Override
