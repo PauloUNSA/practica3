9545b4d32deb50d75983f5ceb0f848f397b8f371
Joel <jchinop@unsa.edu.pe>
2023-06-17 06:27:27 -0500

    clase Item para la cola de prioridad

---
+++ src/Item.java
@@ -0,0 +1,26 @@
+public class Item<E> implements Comparable<Item<E>> {
+
+    private E data;
+    private int priority;
+
+    public Item (E data, int p) {
+        this.data = data;
+        this.priority = p;
+    }
+
+    public E getData () {
+        return this.data;
+    }
+
+    public int getPriority() {
+        return this.priority;
+    }
+
+    @Override
+    public int compareTo(Item<E> e) {
+        if (e == null) {
+            throw new NullPointerException("El argumento no puede ser nulo");
+        }
+        return this.priority-e.getPriority();
+    }
+}
