118feae79e2b41dd912a6b90f223f7adf4c45407
Joel <jchinop@unsa.edu.pe>
2023-06-17 08:03:07 -0500

    metodo front

--- src/PriorityQueueHeap.java
+++ src/PriorityQueueHeap.java
@@ -1,5 +1,27 @@
 // Implementación de la clase PriorityQueueHeap
 // Implementación de la clase PriorityQueueHeap
-class PriorityQueueHeap<T extends Comparable<T>> {
+class PriorityQueueHeap<E extends Comparable<E>> {
+    private MaxHeap<Item<E>> queue;
 
+    public PriorityQueueHeap() {
+        queue = new MaxHeap<>();
+    }
+
+    public void enqueue (E e, int p) {
+        Item<E> temp = new Item<>(e, p);
+        queue.insert(temp);
+    }
+
+    public E dequeue() {
+        return queue.remove().getData();
+    }
+
+    public E front() {
+        return queue.getRoot().getData();
+    }
+
+    public E back() {
+        //completar
+        return null;
+    }
 }
